f27042f279625bec147fea17dba996f589302787
freddylimpens <freddy.limpens@gmail.com>
2017-05-22 12:18:08 +0200

    updated about for fdy + rearanged settings

--- pelicanconf.py
+++ pelicanconf.py
@@ -19,6 +19,30 @@ EXTRA_PATH_METADATA = {
     }
 OUTPUT_PATH = 'output/staging'
 
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+
+# Top Menu Links
+NAVLINKS = (
+	('#page-top', ''),
+	('#portfolio', 'Portfolio'),
+    #('#notes', 'Notes'),
+	('#about', 'About'),
+	('#contact', 'Contact')
+)
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('You can modify those links in your config file', '#'),)
+
+THEME = "../pelican-free-agent-limpica"
+
+# ** all settings below are specific to freeagent-limpica theme **
+
 BOOTSTRAP_FILE = 'bootstrap.min.css'
 CSS_FILE = 'freeagent.css'
 FONTS = 'fonts'
@@ -35,54 +59,19 @@ SCRIPTS = [
     'freeagent.js',
 ]
 
-# Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
-CATEGORY_FEED_ATOM = None
-TRANSLATION_FEED_ATOM = None
-
 DIRECT_TEMPLATES = ['index']
-
-# Top Menu Links
-NAVLINKS = (
-	('#page-top', ''),
-	('#portfolio', 'Portfolio'),
-    #('#notes', 'Notes'),
-	('#about', 'About'),
-	('#contact', 'Contact')
-)
 
 # Portfolio Name
 PORTFOLIO = 'Portfolio'
 # Notes Name
 NOTES = 'Notes'
 
-#Contact form fields sorted by: label, input_type, id, required_validation_,msg
-CONTACT_FIELDS = (
-	['Name', 'text', 'name', 'Please enter your name.'],
-	['Email Address', 'email', 'email','Please enter your email address.' ],
-	['Phone Number', 'tel', 'phone', 'Please enter your phone number.'],
-	['Message', 'textarea', 'message', 'Please enter a message.']
-)
-
 ADDRESS1 = 'FRANCE - ITALY'
 
 # Left column
 ABOUT_1 = 'After graduating as an acoustic engineer, Cécile Picard-Limpens obtained her PhD in Computer Science (INRIA, Sophia-Antipolis) on Modeling and Sound Synthesis for Virtual Environments, then worked at the Haute Ecole de Musique in Genève (CH) and to Numédiart (Mons, BE) on research projects combining sound synthesis and human-machine interaction. '
 # Right column
-ABOUT_2 = 'Freddy Limpens received a PhD from INRIA where he worked on tagging and folksonomies, seeking novel ways to interconnect Social Web and Semantic Web.'
+ABOUT_2 = 'Freddy Limpens is a freelance worker providing consulting, teaching, and complete realizations in web applications, linked data, e-learning, and digital fabrication. After graduating as a mechanical engineer, he completed a PhD in computer science at Inria (studying social web data and Linked Data principles). Since then, he contributes, as a fullstack developper and consultant,  to projects ranging from collaborative web platforms, to e-learning eco-systems and digital fabrication. See <a href="http://limpica.net/fdy/cv/">his cv</a> for more details.'
 # Center
 ABOUT_CENTER = '</br>What we perceive constructs us. We are what we feel, what we see, what we hear. But perception is not a faculty per se, it is printed from what we are.. our observation point is localised by our own body, which, just by filling a volume, might influence the observation itself... so the question loops and never ends. This mere question is endless and simply virtiginuous. We have no aim to answer it, just the desire to explore the complexity of the imbrication. The <em>objectivity</em> of the camera will not overcome the ambiguity of perception. However, we consider the camera as a tool for exploring what we may perceive, a window opened on our subconscience.</a>'
 
-# Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
-
-
-
-# THEME = "../pelican-themes/simple-bootstrap"
-# THEME = "../pelican-themes/photowall"
-# THEME = "../pelican-themes/pure"
-THEME = "../pelican-free-agent-limpica"
-# THEME = "../../minimalist-portfolio"
